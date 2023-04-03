a59a41b904fb4046f12ed54b20db2062ea38e92e
sirclown2009 <126462992+sirclown2009@users.noreply.github.com>
2023-04-03 18:49:39 +0200

    Delete homew6 directory

--- homew6/task3.py
+++
@@ -1,6 +0,0 @@
-lst = [1, 2, "qwerty", "asd", "zxc", 4, 6]
-
-
-filtered_list = list(filter(lambda x: x == int, lst))
-print(list(filtered_list))
-
--- homew6/task4.py
+++
@@ -1,7 +0,0 @@
-def upper(i):
-    return i.upper()
-
-
-lst = ["Niamh", "carmen", "harrY"]
-lst1 = list(map(upper, lst))
-print(lst1)
