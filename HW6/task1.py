798d58a1b97be9b50461ee07719b1fcb2cee980b
sirclown2009 <margarita.lozyan@gmail.com>
2023-04-03 17:41:16 +0100

    .

---
+++ HW6/task1.py
@@ -0,0 +1,15 @@
+def set_equal_numbers():
+    set1 = {1, 2, 3, 4, 5}
+    set2 = {1, 3, 6, 7, 4}
+    return print(set1.intersection(set2))
+
+
+
+def set_unique_numbers():
+    set1 = {1, 2, 3, 4, 5}
+    set2 = {1, 3, 6, 7, 4}
+    return print(set1.difference(set2))
+
+
+set_equal_numbers()
+set_unique_numbers()
---
+++ HW6/task3.py
@@ -0,0 +1,6 @@
+lst = [1, 2, "qwerty", "asd", "zxc", 4, 6]
+
+
+filtered_list = list(filter(lambda x: x == int, lst))
+print(list(filtered_list))
+
